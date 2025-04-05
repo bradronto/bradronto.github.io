e908e6d7eddbcfd9663185fca97ff9350aa95c96
bradronto <brad1168@hotmail.com>
2025-04-04 22:39:26 -0400

    and another..

--- timekeep/src/components/dates.js
+++ timekeep/src/components/dates.js
@@ -108,17 +108,14 @@ const WorkHoursTracker = () => {
   const daysOfWeek = getDatesOfCurrentWeek();
   
   const [workHours, setWorkHours] = useState(
-    daysOfWeek.map(() => ({ start: "7:00 AM", end: "3:00 PM", job: "Lake Mariner Data" }))
+    daysOfWeek.map(() => ({ start: "7:00 AM", end: "3:00 PM", job: "Lake Mariner Data", isChecked: true}))
   );
 
   const handleChange = (index, type, value) => {
     const updatedWorkHours = [...workHours];
-
     updatedWorkHours[index][type] = value;
-    console.log(workHours)
-    console.log(value)
-
     setWorkHours(updatedWorkHours);
+    console.log(workHours[index].isChecked)
   };
 
   const calculateTotalHours = (start, end) => {
@@ -165,10 +162,13 @@ const WorkHoursTracker = () => {
 
    }
 
-   const [isChecked, setIsChecked] = useState(false);
+   //const [isChecked, setIsChecked] = useState(false);
 
-   const handleCheckboxChange = () => {
-     setIsChecked(!isChecked);
+   const handleCheckboxChange = (index, type, value) => {
+    const updatedWorkHours = [...workHours];
+
+    updatedWorkHours[index][type] =  value;
+   // setWorkHours(updatedWorkHours);
    };
  
   return (
@@ -179,15 +179,13 @@ const WorkHoursTracker = () => {
 
     
       <span className="cool-header"> {getMonth()} -- {totalWeeklyHours.toFixed(1)} hrs</span>
-
-      
-      {daysOfWeek.map((day, index) => (
-        <table key={index} style={{ marginLeft: "10px" }}>
+     {daysOfWeek.map((day, index) => (
+       <table key={index} style={{ marginLeft: "10px" }}>
          <tr><td>
          <input
           type="checkbox"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
+          checked={workHours[index].isChecked}
+          onChange={(e) => handleChange(index, "isChecked", !workHours[index].isChecked)}       
         />
 
             <NumberInBox day={day} color="grey" />
@@ -198,38 +196,47 @@ const WorkHoursTracker = () => {
          
             </td>
             <td>
-            
+       
           <table><tr><td>
+          <SelectWithNewItem  myArray={optionsArray} />
+       <select  className="cool-time-select"// style={{ marginRight: "0px" }} 
+        defaultValue={timeOptions['14']}
+        //defaultValue={workHours[index].start}
+        onChange={(e) => handleChange(index, "start", e.target.value)}
+                
+        >
+         {timeOptions.map((time, indx) => (
+        <option  key={indx} value={time}>
+          {time}
+        </option>
+      ))}
+        </select>         
+          <select className="cool-time-select" //style={{ marginRight: "0px" }} 
+        defaultValue={timeOptions['30']}
+        //defaultValue={workHours[index].end}
+        onChange={(e) => handleChange(index, "end", e.target.value)}
+               
+        >
+         {timeOptions.map((time, indx) => (
+        <option  key={indx} value={time}>
+          {time}
+        </option>
+      ))}
+        </select> 
+         </td></tr></table>
 
-            <SelectWithNewItem  myArray={optionsArray} />
-         <select  className="cool-time-select"// style={{ marginRight: "0px" }} 
-          defaultValue={timeOptions['14']}
-          //defaultValue={workHours[index].start}
-          onChange={(e) => handleChange(index, "start", e.target.value)}
-                  
-          >
-           {timeOptions.map((time, indx) => (
-          <option  key={indx} value={time}>
-            {time}
-          </option>
-        ))}
-          </select>         
-            <select className="cool-time-select" //style={{ marginRight: "0px" }} 
-          defaultValue={timeOptions['30']}
-          //defaultValue={workHours[index].end}
-          onChange={(e) => handleChange(index, "end", e.target.value)}
-                 
-          >
-           {timeOptions.map((time, indx) => (
-          <option  key={indx} value={time}>
-            {time}
-          </option>
-        ))}
-          </select> 
-           </td></tr></table>
+
+        
+      
+        
+        
+        
+
          
           </td> </tr> 
           </table>
+      
+
       ))}
       <br></br>
       <a  onClick={setraw}    href={`sms:?&body=${encodeURIComponent(rawText)}`}>
@@ -256,8 +263,11 @@ const WorkHoursTracker = () => {
             
 
    
-      </div></div>
-  );
+      </div>
+  </div>
+  
+
+);
 };
 
 const NumberInBox = props => {
